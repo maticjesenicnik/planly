4d08b671bed5e66c5ed12d3ebd31c6980ac06e38
maticjesenicnik <matic.jesenicnik@gmail.com>
2026-05-26 08:07:14 +0200

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Planly')
+    .setDescription(
+      'Planly helps teams organize projects, track tasks with flexible workflows, and collaborate efficiently. Features include JWT authentication, role-based access, task assignment, comments, file attachments, status workflows, and background reminders.',
+    )
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
